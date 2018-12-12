9fe60b2d5436195746ef39e7c9a96743e6a6aad3
jieyuanfei <jieyuanfei@sina.com>
2018-12-12 17:52:47 +0800

    新增文章列表功能

--- src/pages/article/articleList.vue
+++ src/pages/article/articleList.vue
@@ -2,14 +2,16 @@
   <div class="list-container">
     <ul class="note-list">
       <li class="have-img" v-for="item in articleList">
-        <a href="#" class="wrap-img"><img :src="item.img" alt="" class="img-blur-done"></a>
+        <a @click.prevent class="wrap-img"><img src="../../../static/img/author-newestCommand1.png" alt="" class="img-blur-done"></a>
         <div class="content">
-          <a href="#" class="title" v-text="item.title"></a>
-          <p class="abstract" v-text="item.content"></p>
+          <router-link :to="{path:'/articleDetail',query:{id:item.id}}" class="title">
+            {{item.title}}
+          </router-link>
+          <p class="abstract" v-text="item.text"></p>
           <div class="meta">
-            <a href="#" class="nickname" v-text="item.author"></a>
-            <a href="#" v-text="item.command"><i class="iconfont ic-list-comments"></i></a>
-            <span v-text="item.like"><i class="iconfont ic-list-like"></i></span>
+            <a @click.prevent class="nickname">{{item.t_user.username}}</a>
+            <a @click.prevent v-text="item.comment_num"><i class="iconfont ic-list-comments"></i></a>
+            <span v-text="item.like_num"><i class="iconfont ic-list-like"></i></span>
           </div>
         </div>
       </li>
